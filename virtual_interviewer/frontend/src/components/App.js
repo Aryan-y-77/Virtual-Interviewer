ef8619bd01cd1df7990c8f9e706107caed8ea283
meekhumor <om17092004@gmail.com>
2024-12-21 15:21:06 +0530

    update frontend

--- virtual_interviewer/frontend/src/components/App.js
+++ virtual_interviewer/frontend/src/components/App.js
@@ -1,25 +1,11 @@
-import React, { Component } from "react";
-import { createRoot } from "react-dom/client"; 
-import { BrowserRouter as Router } from "react-router-dom"; 
-import HomePage from "./HomePage";
-import '../../static/css/index.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home/Home";
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <Router> 
-        <div>
-          <HomePage />
-        </div>
-      </Router>
-    );
-  }
-}
-
-const appDiv = document.getElementById("app");
-const root = createRoot(appDiv); // Correct method for React 18+
-root.render(<App />);
+export default function HomePage(){
+  return (
+    <Routes>
+        <Route path="/" element={<Home />} />
+    </Routes>
+  );
+};
--- virtual_interviewer/frontend/src/index.js
+++ virtual_interviewer/frontend/src/index.js
@@ -1,1 +1,13 @@
-import App from "./components/App";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './components/App';
+import '../static/css/index.css'; 
+
+const appDiv = document.getElementById("app");
+const root = createRoot(appDiv);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
