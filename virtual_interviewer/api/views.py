148da25f8eaf78aefa8e34c97bd56acdfcdc731d
meekhumor <om17092004@gmail.com>
2024-12-21 10:31:56 +0530

    create backend

--- virtual_interviewer/api/views.py
+++ virtual_interviewer/api/views.py
@@ -3,6 +3,9 @@ from rest_framework import generics
 from .serializers import RoomSerializer
 from .models import Room
 
-class RoomView(generics.ListAPIView):
+# Create your views here.
+
+
+class RoomView(generics.CreateAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
